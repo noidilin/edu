822c4b69768c30460e76c283781fe17a6f7deaf3
noidilin <linganinja.0120@gmail.com>
2025-09-06 11:58:28 +0800

    feat(logo): add motion to logo-ticker section

--- app/(home)/logo-ticker.tsx
+++ app/(home)/logo-ticker.tsx
@@ -8,6 +8,8 @@ import outsideLogo from '@/public/images/outside.svg'
 import pulseLogo from '@/public/images/pulse.svg'
 import quantumLogo from '@/public/images/quantum.svg'
 import twiceLogo from '@/public/images/twice.svg'
+import { Fragment } from 'react'
+import * as motion from 'motion/react-client'
 
 const logos = [
   { name: 'Quantum', image: quantumLogo },
@@ -27,11 +29,19 @@ export default function LogoTicker() {
         Already chosen by these market leaders
       </H4>
       <div className='mt-12 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
-        <div className='flex gap-24 pr-24'>
-          {logos.map((logo) => (
-            <Image alt={logo.name} key={logo.name} src={logo.image} />
+        <motion.div
+          animate={{ x: '-50%' }}
+          transition={{ duration: 30, ease: 'linear', repeat: Infinity }}
+          className='flex gap-24 pr-24'
+        >
+          {Array.from({ length: 2 }).map((_, i) => (
+            <Fragment key={i}>
+              {logos.map((logo) => (
+                <Image alt={logo.name} key={logo.name} src={logo.image} />
+              ))}
+            </Fragment>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   )
